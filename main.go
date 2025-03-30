82bd5f2d239e2dd3e10da6570baa6d0eb52808cd
samuel bonu <samuelbonux10@gmail.com>
2025-03-30 14:25:17 +0100

    transaction body response

--- main.go
+++ main.go
@@ -1,14 +1,3 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/h3th-IV/go-paysterk/paysterk"
-)
-
-func main() {
-	http.HandleFunc("/go-paysterk", paysterk.WebHookHandler)
-	log.Println("webhook server running and listening on :9090")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+func main() {}
